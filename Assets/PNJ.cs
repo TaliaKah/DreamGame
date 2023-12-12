94ebcf6956c42175a00c6ecba982d8c11cad1206
TaliaKah <anaissaratagougne@tutanota.com>
2023-12-13 00:39:50 +0100

    Add PNJ with TODO

---
+++ Assets/PNJ.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PNJ : MonoBehaviour
+{
+    public string PNJName;
+
+    public string description;
+
+    // TODO Notes: 
+    // -> gérer tous les trucs communs (assets, déplacements, position (le tout en focntion des niveaux))
+    // -> pour la position avoir un gameObject du niveau en entrée qui donnera la position
+    // -> pour les déplacements utiliser un script en entrée qui sera la fonction pour bouger ou alors avoir un script spécial pour le déplacment en dehors de la classe et dedans aoir quelquechose pour déterminer le comportement du personnage
+    // -> avoir différentes descriptions en fonction des niveaux, elles aparaissent au fur et à mesure quand on voudra faire apparaître les informations
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
