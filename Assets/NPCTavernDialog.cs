722096385070644004d54e2d03e724d991a0d24f
TaliaKah <anaissaratagougne@tutanota.com>
2024-01-10 00:42:20 +0100

    Add tavern dialogs

--- Assets/NPCTavernDialog.cs
+++ Assets/NPCTavernDialog.cs
@@ -4,12 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
 
-public class NPCKnightDialog : MonoBehaviour
+public class NPCTavernDialog : MonoBehaviour
 {
     public NPCConversation firstConversation;
-    public NPCConversation secondConversation;
+    public NPCConversation casualConversation;
     private DecisionManager decisionManager;
-    // ajouter conversattion qui invite à prendre un verre dans la taverne et à parler au tavernier
 
     void Start()
     {
@@ -22,13 +21,13 @@ public class NPCKnightDialog : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
-                if (!decisionManager.GetDecision(DecisionManager.Decision.RencontrerLesChevaliersDansLaPlaine))
+                if (!decisionManager.GetDecision(DecisionManager.Decision.PrendreUneBoissonALaTaverne))
                 {
-                    ConversationManager.Instance.StartConversation(firstConversation);
+                    ConversationManager.Instance.StartConversation(casualConversation);
                 }
                 else
                 {
-                    ConversationManager.Instance.StartConversation(secondConversation);
+                    ConversationManager.Instance.StartConversation(firstConversation);
                     ConversationManager.Instance.SetBool("EtreChevalier", decisionManager.GetDecision(DecisionManager.Decision.AccepterDeDevenirChevalier));
                 }
             }
