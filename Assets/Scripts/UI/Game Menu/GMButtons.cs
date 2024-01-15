9b295577224fa2f8ee7aff10ff03f6bdbed7c7ff
Guy Luong <guy.luong.7@gmail.com>
2024-01-15 15:00:54 +0100

    [UI] Game menu script to complete

---
+++ Assets/Scripts/UI/Game Menu/GMButtons.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GMButtons : MonoBehaviour
+{
+    public void Resume()
+    {
+        // TODO
+    }
+
+    public void ShowOptions()
+    {
+        // TODO
+    }
+
+    public void Save()
+    {
+        // TODO
+    }
+
+    public void Load()
+    {
+        // TODO
+    }
+
+    public void MainMenu()
+    {
+        // TODO
+    }
+}
