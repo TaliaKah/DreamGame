151beb5f01e1685d133b68bdc374005d5b34925c
TaliaKah <anaissaratagougne@tutanota.com>
2024-01-27 20:15:54 +0100

    [GameManager] Add delay before credits

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -16,12 +16,43 @@ public class GameManager : MonoBehaviour
             ConversationManager.Instance.StartConversation(startConversation);
         }
     }
+    public float delayBeforeLoad = 3f; 
+    public Image fadeImage;
+    public float fadeDuration = 1f;
 
     private void Update()
     {
         if (decisionManager.GetDecision(DecisionManager.Decision.SeReveiller))
         {
-            SceneLoaderAsync.Instance.LoadScene("Credits");
+            StartCoroutine(LoadSceneWithDelay("Credits"));
         }
+    }
+
+    IEnumerator LoadSceneWithDelay(string sceneName)
+    {
+        // Fondu au noir
+        StartCoroutine(FadeToBlack());
+
+        // Attente
+        yield return new WaitForSeconds(delayBeforeLoad);
+
+        // Chargement de la scène
+        SceneLoaderAsync.Instance.LoadScene(sceneName);
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        float elapsedTime = 0f;
+        Color originalColor = fadeImage.color;
+        Color targetColor = Color.black;
+
+        while (elapsedTime < fadeDuration)
+        {
+            fadeImage.color = Color.Lerp(originalColor, targetColor, elapsedTime / fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        fadeImage.color = targetColor;
     }
 }
