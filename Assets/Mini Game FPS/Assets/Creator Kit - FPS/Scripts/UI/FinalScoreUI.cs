c222e2a975b9a25ca7ad8a219620668f7d55c942
Fady's Coding <algerienvenere@gmail.com>
2024-01-28 17:04:17 +0100

    load scene + settings between scenes fixed

---
+++ Assets/Mini Game FPS/Assets/Creator Kit - FPS/Scripts/UI/FinalScoreUI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalScoreUI : MonoBehaviour
+{
+    public static FinalScoreUI Instance { get; private set; }
+
+    public Text WinningTitle;
+    public Text LosingTitle;
+    public Text TargetDestroyed;
+    public Text Penalty;
+    public Text TimeSpent;
+    public Text FinalTime;
+    public Text FinalScore;
+    
+    void Awake()
+    {
+        Instance = this;
+        gameObject.SetActive(false);
+    }
+
+    public void Display()
+    {
+        gameObject.SetActive(true);
+
+        LosingTitle.gameObject.SetActive(false);
+        WinningTitle.gameObject.SetActive(true);
+        
+        float time = GameSystem.Instance.RunTime;
+        int targetDestroyed = GameSystem.Instance.DestroyedTarget;
+        int totalTarget = GameSystem.Instance.TargetCount;
+        int missedTarget = totalTarget - targetDestroyed;
+        float penaltyAmount = GameSystem.Instance.TargetMissedPenalty * missedTarget;
+            
+        TargetDestroyed.text = targetDestroyed + "/" + totalTarget;
+        TimeSpent.text = time.ToString("N2") + "s";
+        Penalty.text = missedTarget +"*"+ GameSystem.Instance.TargetMissedPenalty.ToString("N2")+"s = "+ penaltyAmount.ToString("N2") + "s";
+        FinalTime.text = (time + penaltyAmount).ToString("N2") + "s";
+
+        FinalScore.text = GameSystem.Instance.Score.ToString("N");
+    }
+
+
+    public void DisplayLost()
+    {
+        Debug.Log("You must survive at least 10 seconds before reaching the end checkpoint!");
+        gameObject.SetActive(true);
+
+        LosingTitle.gameObject.SetActive(true);
+        WinningTitle.gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Mini Game FPS/Assets/Creator Kit - FPS/Scripts/UI/RestartLevel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void NextLevel()
+    {
+        UIAudioPlayer.PlayPositive();
+       //GameSystem.Instance.NextLevel();
+       //SceneManager.LoadScene("Example Scene");
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
