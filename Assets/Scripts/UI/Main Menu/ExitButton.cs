018d1c6fae0cd046c3975c208dd6645c203fab4a
Guy Luong <guy.luong.7@gmail.com>
2024-01-28 17:40:36 +0100

    [Exit] Quitting app in Editor

--- Assets/Scripts/UI/Main Menu/ExitButton.cs
+++ Assets/Scripts/UI/Main Menu/ExitButton.cs
@@ -7,6 +7,10 @@ public class ExitButton : MonoBehaviour
 {
     public void Exit()
     {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
