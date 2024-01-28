ef8d792be285d35c1a1cd7aa09eb6a79ccec9447
TaliaKah <anaissaratagougne@tutanota.com>
2024-01-28 20:42:58 +0100

    [PNJ] Smooth LookAt

--- Assets/Scripts/NPC/PNJ.cs
+++ Assets/Scripts/NPC/PNJ.cs
@@ -14,7 +14,10 @@ public class PNJ : MonoBehaviour
     {
         if (playerTransform != null)
         {
-            transform.LookAt(playerTransform);
+            Vector3 directionToLook = playerTransform.position - transform.position;
+            Quaternion targetRotation = Quaternion.LookRotation(directionToLook);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
         }
     }
 }
