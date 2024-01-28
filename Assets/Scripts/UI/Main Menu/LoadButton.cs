55d0d002edaf72cd40bc18912b1a99b675a4314c
Guy Luong <guy.luong.7@gmail.com>
2024-01-28 22:04:19 +0100

    [Save] UI Scripts (1/2)

---
+++ Assets/Scripts/UI/Main Menu/LoadButton.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LoadButton : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        // if (SaveManager.Instance.load()) {
+        //     SceneLoaderAsync.Instance.LoadScene("SampleScene");
+        // } else {
+        //     Debug.Log("Game load attempted in Main Menu.");
+        // }
+    }
+}
