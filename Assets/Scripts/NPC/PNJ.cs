a1dc5ce63836c1b6ec9d7cbfe46441d6c4aafb07
TaliaKah <anaissaratagougne@tutanota.com>
2024-01-29 15:53:32 +0100

    [PNJ] Resolve LookAtUs problems + Resolve main menu buttons

--- Assets/Scripts/NPC/PNJ.cs
+++ Assets/Scripts/NPC/PNJ.cs
@@ -19,7 +19,7 @@ public class PNJ : MonoBehaviour
             Vector3 directionToLook = playerTransform.position - transform.position;
             Quaternion targetRotation = Quaternion.LookRotation(directionToLook);
 
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+            transform.rotation = targetRotation;
         }
     }
 }
